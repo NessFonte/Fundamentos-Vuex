fc350b181a3ed7e098a7a65a5c6dc55c5c2336c8
neisser <neisser2909@gmail.com>
2023-05-19 23:16:46 -0400

    Creadas mutaciones para aumentar el valor del state

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,16 +2,32 @@
     <h1>Counter - Vuex</h1>
     <h2>Direct access: {{ $store.state.count }}</h2>
 
+    <button @click="increment">+1</button>
+    <button @click="incrementBy">+5</button>
+    <button>Random</button>
+
     <h1>mapState</h1>
     <h2>mapState: {{ count }}</h2>
+    <h2>mapState: {{ lastMutation }}</h2>
 </template>
 
 <script>
 import { mapState } from "vuex"
 
 export default {
-    computed: mapState(['count'])
+    computed: {
+        ...mapState(['count', 'lastMutation'])
+    },
     
+    methods: {
+        increment() {
+            this.$store.commit('increment')
+        },
+
+        incrementBy() {
+            this.$store.commit('incrementBy', 5)
+        }
+    }
 
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,22 @@
 import { createStore } from "vuex"
 
 export default createStore({
+    // Variables reactivas de Vuex
     state: {
-        count: 1
+        count: 1,
+        lastMutation: 'none',
+    },
+
+    // Cambio de estado de los states en Vuex
+    mutations: {
+        increment(state) {
+            state.count++
+            state.lastMutation = 'increment'
+        },
+
+        incrementBy(state, value) {
+            state.count += value
+            state.lastMutation = 'incrementBy'
+        }
     }
 })
