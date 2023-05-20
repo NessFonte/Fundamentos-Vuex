aaafb9d9b97afc695d1ae8d166eb2941d3fec902
neisser <neisser2909@gmail.com>
2023-05-19 23:04:55 -0400

    Creado componente Counter para leer el state

---
+++ src/components/Counter.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Counter - Vuex</h1>
+    <h2>Direct access: {{ $store.state.count }}</h2>
+
+    <h1>mapState</h1>
+    <h2>mapState: {{ count }}</h2>
+</template>
+
+<script>
+import { mapState } from "vuex"
+
+export default {
+    computed: mapState(['count'])
+    
+
+}
+</script>
